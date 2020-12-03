4f7c184750394a333e60f70ebc797e0fb128005d
Ulyuly <k_tuszynski@vp.pl>
2020-12-03 14:22:52 +0100

    Organizing files into folders, no major changes

---
+++ chiveman/tuszynski_kamil/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="tuszynski_kamil.php"  method="post">
+<input type="text" name="name" placeholder="imię" autofocus><br><br>
+<input type="text" name="surname" placeholder="nazwisko"><br><br>
+<input type="number" name="a" placeholder="a"><br><br>
+<input type="number" name="b" placeholder="b"><br><br>
+<input type="submit" value="Wyślij dane">
+</form>
+</body>
+</html>
